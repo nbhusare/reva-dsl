3210fe73da72f3512f17aee2f980a5dfd73e08a1
nbhusare <neerajbhusare@gmail.com>
2024-05-31 20:57:28 +0530

    43: Removed Lombok Dependency

--- reva.ide/src/main/java/reva/ide/contentassist/providers/Snippet.java
+++ reva.ide/src/main/java/reva/ide/contentassist/providers/Snippet.java
@@ -1,13 +1,31 @@
 package reva.ide.contentassist.providers;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@NoArgsConstructor
 public class Snippet {
-    private @Setter String insertText;
-    private @Setter String label;
-    private @Setter int priority;
+	private String insertText;
+	private String label;
+	private int priority;
+
+	public String getInsertText() {
+		return insertText;
+	}
+
+	public void setInsertText(String insertText) {
+		this.insertText = insertText;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
 }
--- reva.ide/src/test/java/reva/ide/LanguageServerTestUtils.java
+++ reva.ide/src/test/java/reva/ide/LanguageServerTestUtils.java
@@ -1,10 +1,5 @@
 package reva.ide;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,62 +8,68 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Slf4j
+import org.junit.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class LanguageServerTestUtils {
-  private LanguageServerTestUtils() {}
 
-  /**
-   * Get the file content as a Json object.
-   *
-   * @param filePath to read the content
-   * @return {@link JsonObject}
-   */
-  public static JsonObject fileContentAsObject(Path filePath) {
-    return JsonParser.parseString(getFileContent(filePath)).getAsJsonObject();
-  }
+	private static final Logger log = LoggerFactory.getLogger(LanguageServerTestUtils.class);
 
-  /**
-   * Get the file content.
-   *
-   * @param filePath to read the content
-   * @return {@link String}
-   */
-  public static String getFileContent(Path filePath) {
-    String contentAsString = "";
-    try {
-      contentAsString = new String(Files.readAllBytes(filePath));
-    } catch (IOException ex) {
-      log.error(ex.getMessage());
-    }
+	private LanguageServerTestUtils() {
+	}
 
-    return contentAsString;
-  }
+	/**
+	 * Get the file content as a Json object.
+	 *
+	 * @param filePath to read the content
+	 * @return {@link JsonObject}
+	 */
+	public static JsonObject fileContentAsObject(Path filePath) {
+		return JsonParser.parseString(getFileContent(filePath)).getAsJsonObject();
+	}
 
-  public static List<String> getTestConfigs(Path testRoot, String configDir) {
-    try {
-      return Files.walk(testRoot.resolve(configDir))
-          .filter(
-              path -> {
-                File file = path.toFile();
-                return file.isFile() && file.getName().endsWith(".json");
-              })
-          .map(path -> path.toFile().getName())
-          .collect(Collectors.toList());
-    } catch (IOException e) {
-      // If failed to load tests, then it's a failure
-      Assert.fail("Unable to load test configs");
-      return Collections.emptyList();
-    }
-  }
+	/**
+	 * Get the file content.
+	 *
+	 * @param filePath to read the content
+	 * @return {@link String}
+	 */
+	public static String getFileContent(Path filePath) {
+		String contentAsString = "";
+		try {
+			contentAsString = new String(Files.readAllBytes(filePath));
+		} catch (IOException ex) {
+			log.error(ex.getMessage());
+		}
 
-  public static void updateConfig(
-      String actual, JsonObject configJson, Path testRoot, String configDir, String config) {
-    configJson.addProperty("expected", actual);
-    Path path = testRoot.resolve(configDir).resolve(config);
-    try {
-      Files.writeString(path, configJson.toString());
-    } catch (IOException e) {
-      // Ignore the file write
-    }
-  }
+		return contentAsString;
+	}
+
+	public static List<String> getTestConfigs(Path testRoot, String configDir) {
+		try {
+			return Files.walk(testRoot.resolve(configDir)).filter(path -> {
+				File file = path.toFile();
+				return file.isFile() && file.getName().endsWith(".json");
+			}).map(path -> path.toFile().getName()).collect(Collectors.toList());
+		} catch (IOException e) {
+			// If failed to load tests, then it's a failure
+			Assert.fail("Unable to load test configs");
+			return Collections.emptyList();
+		}
+	}
+
+	public static void updateConfig(String actual, JsonObject configJson, Path testRoot, String configDir,
+			String config) {
+		configJson.addProperty("expected", actual);
+		Path path = testRoot.resolve(configDir).resolve(config);
+		try {
+			Files.writeString(path, configJson.toString());
+		} catch (IOException e) {
+			// Ignore the file write
+		}
+	}
 }
